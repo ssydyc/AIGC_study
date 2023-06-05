7d38775362e91abf888f1dea069a0a65619d918b
Samsong <ssydyc@gmail.com>
2023-06-04 20:40:05 -0700

    add notebook and code of lora fine tune

---
+++ fine_tune_chatGLM/tokenize_dataset_rows.py
@@ -0,0 +1,19 @@
+import argparse
+import json
+from tqdm import tqdm
+
+import datasets
+import transformers
+
+
+def preprocess(tokenizer, config, example, max_seq_length):
+    prompt = example["context"]
+    target = example["target"]
+    prompt_ids = tokenizer.encode(prompt, max_length=max_seq_length, truncation=True)
+    target_ids = tokenizer.encode(
+        target,
+        max_length=max_seq_length,
+        truncation=True,
+        add_special_tokens=False)
+    input_ids = prompt_ids + target_ids + [config.eos_token_id]
+    return {"input_ids": input_ids, "seq_len": len(prompt_ids)}
